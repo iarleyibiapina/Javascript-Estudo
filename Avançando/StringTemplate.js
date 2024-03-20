d1ccce488e3feeb84638b50498f525fa0ecac8dd
iarley <7iarley7@gmail.com>
2024-03-20 00:25:27 -0300

    string template

---
+++ Avançando/StringTemplate.js
@@ -0,0 +1,15 @@
+//===================String Template - Usar HTML|String junto com javascript==================//
+//
+//=====================================//
+
+// Definida pela `` (crase)
+// Chama variaveis por meio do ${}
+
+let template = "timao";
+let basicao = 300;
+
+console.log(`meu ${template} tem ${basicao} torcedores`);
+
+// Muito utilizado com o DOM para criar ou modificar elementos HTML.
+
+//=============================================================================//
