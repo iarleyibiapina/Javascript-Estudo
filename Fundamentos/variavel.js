c95449df7cbbe151bae392039c5aa9f7e05ee189
iarley <7iarley7@gmail.com>
2024-03-24 21:29:04 -0300

    variavel

---
+++ Fundamentos/variavel.js
@@ -0,0 +1,18 @@
+//===================VARIAVEIS==================//
+//
+// Em js as variaveis sao case sensitivity
+//
+//===================VAR==================//
+// forma 'primitiva' do js, geralmente usada em escopos globais
+var umaVariavel = 'a';
+//===================LET==================//
+// let surgiu com a nova versao do js, usada geralemente em 'escopos' 
+// para uma melhor dinamica e modulaçao do codigo.
+let outraVariavel = 'b';
+//===================CONST==================//
+// Em consts, não é possivel alterar seu valor - geralmente usado como uma forma de 'Tipar' o dado
+const naoAlteraValor = 10;
+// console.log(naoAlteraValor++); // não é possivel;
+//=============================================================================//
+
+
