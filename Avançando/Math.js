a95e7bd06b07671fd7d719f464fa464c6d951ce1
iarley <7iarley7@gmail.com>
2024-03-30 18:52:43 -0300

    Math

---
+++ Avançando/Math.js
@@ -0,0 +1,13 @@
+//===================MATH==================//
+// Uma biblioteca do Javascript feita para lidar com matematica
+// arredondar, formulas matemáticas, contantes matemáticas
+
+Math.random(); // Gera numero aleatório entre 0 e 1 (sendo 0.0000000 e 1.000000)
+Math.max(); // Retorna maior valor
+Math.min(); // Retorna menor valor
+Math.PI(); // Retorna o Pi matemático
+Math.SQRT2(); // Retorna a raiz quadrada
+
+// entre uma lista enorme de propriedades e métodos...
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math
+//=============================================================================//
