c83588008fc33953059ce140b581150c695ace03
iarley <7iarley7@gmail.com>
2024-03-30 15:51:05 -0300

    Mini Projetos - Radar

---
+++ MiniProjetos/Radar.js
@@ -0,0 +1,34 @@
+// verificar velocidade e aplica multa
+// velocidade máxima de 50 Km/h
+// cada 5km acima da média aplicar 2 ponto na multa
+// Math.floor()
+// Caso pontos maior que 10 => suspender a carteira
+
+// casos a se testar
+// 40 - 50 - 60 - 75 - 88.88888
+let carteiraSuspensa = false;
+let pontosCarteira = 0;
+let velocidade = radar(56.867);
+function radar(velocidade)
+{
+    // arredondanddo velocidade - para abaixo de .5
+    velocidade = Math.floor(velocidade);
+    // 
+    if(velocidade <= 50) return "Velocidade abaixo do permitido";
+    //
+    let velocidadeAcima = velocidade - 50;
+    console.log(`Velocidade ${velocidadeAcima}Km/h acima do permitido`);
+    //
+    let definirPontos = Math.floor((velocidadeAcima / 5) * 2);
+    pontosCarteira += definirPontos;
+    console.log(`Será aplicado ${definirPontos} pontos na carteira`);
+    //
+    if(definirPontos >= 10) {
+        carteiraSuspensa = true;
+        console.log(`Pontos na carteira excedeu 10, sua carteira será suspendida`);
+    }
+    //
+    return `Carteira suspensa: ${carteiraSuspensa}, Pontos na carteira ${pontosCarteira}`;
+}
+
+console.log(velocidade);
