046b423d5b6b5468345ed55fe454cc95f3706eac
iarley <7iarley7@gmail.com>
2024-03-30 15:03:02 -0300

    MiniProjeto - Achar maior valor

--- MiniProjetos/MaiorNumero.js
+++ MiniProjetos/MaiorNumero.js
@@ -1,8 +1,4 @@
-// ;
-
-// recebe como param um: int, array ou object;
-// e retorna maior valor
-
+// Achar maior valor em um elemento
 
 /**
  * Retorna o maior numero entre dois numeros passados como param
@@ -14,28 +10,38 @@ function maiorValorEntreDoisInteiros(valorUm, valorDois)
 {
     return valorUm > valorDois ?  valorUm : valorDois;
 }
-console.log(maiorValorEntreDoisInteiros(40,20));
+// console.log(maiorValorEntreDoisInteiros(40,20));
 
+// acha maior valor em array
+let aArr = [10, 15, 5, 0, 20, 30, 10];
 /**
  * Retorna o maior numero em um array
  * @param {Array} elemento 
  * @returns {int} value - maior valor encontrado
 */
-// acha maior valor em array
-let aArr = [10];
 function maiorValorEmArray(elemento) {
-
+    let temporaria = elemento[0];
+    for (valor of elemento) {
+        temporaria < valor ? temporaria = valor : temporaria;
+    }
+    return temporaria;
 }
-// console.log(maiorValor(aArr));
+// console.log(maiorValorEmArray(aArr));
 
+// acha maior valor em objeto
+let aObj = {primeiro: 10, segundo: 15, terceiro: 120 , quarto: 0};
 /**
  * Retorna o maior numero em um objeto
  * @param {object} elemento 
  * @returns {int} value - maior valor encontrado
 */
-// acha maior valor em objeto
-let aObj = {chave: 10};
 function maiorValorEmObjeto(elemento){
-    
+    // esta função pega a primeira chave do objeto
+    let primeiraChaveObjeto = Object.keys(elemento)[0];
+    let temporaria = elemento[primeiraChaveObjeto];
+    for (valor in elemento) {
+        temporaria < elemento[valor] ? temporaria = elemento[valor] : temporaria;
+    }
+    return temporaria;
 }
-// console.log(maiorValor(aObj));
+// console.log(maiorValorEmObjeto(aObj));
