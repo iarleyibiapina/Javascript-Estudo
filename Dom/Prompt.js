a44b93f0b5383933cc7a27991492b1e1504d6818
iarley <7iarley7@gmail.com>
2024-03-31 00:20:54 -0300

    Prompt

---
+++ Dom/Prompt.js
@@ -0,0 +1,10 @@
+//===================PROMPT==================//
+// Abre uma caixa de dialogo, o usuario digita e o valor é armazenado em uma variavel
+let cor = prompt(`Qual sua cor?`);
+
+// abre uma caixa de alerta
+// alert();
+
+// abre uma caixa com uma mensagem e um botão de confirmação que pode ser usado na lógica
+// confirm();
+//=============================================================================//
